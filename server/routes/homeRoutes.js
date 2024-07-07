8f57443275a7e768b35cda573f3f123f0b45558f
David <davidolanrewaju538@gmail.com>
2024-07-08 00:57:40 +0100

    Update home route

--- server/routes/homeRoutes.js
+++ server/routes/homeRoutes.js
@@ -5,6 +5,12 @@ const router = express.Router();
 router.get('/', (req, res) => {
     res.json({
         message: "Welcome!",
+        routes: {
+            authentication: {
+                register: "https://stage-two-brown.vercel.app/auth/register",
+                login: "https://stage-two-brown.vercel.app/auth/login"
+            },
+        }
     });
 });
 
