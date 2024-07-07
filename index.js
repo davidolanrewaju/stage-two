d3312d87925417574dd93a37b204ab4ea5c4b811
David <davidolanrewaju538@gmail.com>
2024-07-07 17:51:46 +0100

    Fix route

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 // server.js
 import dotenv from "dotenv";
-import app from "./server/app.js";
+// import app from "./server/app.js";
+import express from "express";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+
+import authRoutes from "./server/routes/authRoutes.js";
 
 dotenv.config();
 const port = process.env.PORT || 3000;
+
+
+const app = express();
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/auth', authRoutes);
+
+app.get('/', (req, res) => {
+    res.json({
+        message: "Welcome!",
+    });
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
