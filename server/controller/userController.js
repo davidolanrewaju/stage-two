683573278759ebf3c31a438ad4ede8bcc5e620e6
David <davidolanrewaju538@gmail.com>
2024-07-09 11:38:16 +0100

    File change

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -3,28 +3,73 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export const getUserById = async (req, res) => {
-  const { id } = req.params;
+  const { id: userId } = req.params;
+  const loggedInUserId = req.user.id;
 
   try {
-    const user = await prisma.user.findUnique({ where: { id } });
+    if (userId === loggedInUserId) {
+      const user = await prisma.user.findUnique({ where: { id: userId } });
 
-    if (!user) {
+      if (!user) {
+        return res.status(404).json({
+          status: "Not Found",
+          message: "User not found",
+          statusCode: 404,
+        });
+      }
+
+      return res.status(200).json({
+        status: "success",
+        message: "User retrieved successfully",
+        data: {
+          userId: user.id,
+          firstName: user.firstName,
+          lastName: user.lastName,
+          email: user.email,
+          phone: user.phone,
+        },
+      });
+    }
+
+    const organisations = await prisma.organisation.findMany({
+      where: {
+        members: {
+          some: {
+            userId: loggedInUserId,
+          },
+        },
+      },
+      include: {
+        members: true,
+      },
+    });
+
+    let userFound = null;
+    for (const org of organisations) {
+      const user = org.members.find((member) => member.userId === userId);
+      if (user) {
+        userFound = await prisma.user.findUnique({ where: { id: userId } });
+        break;
+      }
+    }
+
+    if (!userFound) {
       return res.status(404).json({
         status: "Not Found",
         message: "User not found",
         statusCode: 404,
       });
     }
-
+    
     res.status(200).json({
       status: "success",
       message: "User retrieved successfully",
       data: {
-        userId: user.id,
-        firstName: user.firstName,
-        lastName: user.lastName,
-        email: user.email,
-        phone: user.phone,
+        userId: userFound.id,
+        firstName: userFound.firstName,
+        lastName: userFound.lastName,
+        email: userFound.email,
+        phone: userFound.phone,
       },
     });
   } catch (error) {
